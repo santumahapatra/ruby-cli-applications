e1fc541370cb61315d66fc00c3f4cdf249c38d0d
Santu Mahapatra <santumahapatra@gmail.com>
2014-07-04 23:52:33 +0530

    Making the applications to work with only one application from the command line

--- 1_backing_up_data/db_backup.rb
+++ 1_backing_up_data/db_backup.rb
@@ -1,29 +1,15 @@
 #!/usr/bin/env ruby
 
-databases = {
-  :big_client => {
-    :database => 'big_client',
-    :username => 'big',
-    :password => 'big',
-  },
-  :small_client => {
-    :database => 'small_client',
-    :username => 'small',
-    :password => 'p@ssWord!',
-  }
-}
-
+database = ARGV.shift
+username = ARGV.shift
+password = ARGV.shift
 end_of_iter = ARGV.shift
 
-databases.each do |name, config|
-  if end_of_iter.nil?
-    backup_file = config[:database] + '_' + Time.now.strftime('%Y%m%d')
-  else
-    backup_file = config[:database] + '_' + end_of_iter
-  end
-  mysqldump = "mysqldump -u#{config[:username]} -p#{config[:password]}" + 
-  "#{config[:database]}"
 
-  `#{mysqldump} > #{backup_file}.sql`
-  `gzip #{backup_file}.sql`
+if end_of_iter.nil?
+  backup_file = database + Time.now.strftime('%Y%m%d')
+else
+  backup_file = database + end_of_iter
 end
+`mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
+`gzip #{backup_file}.sql`
